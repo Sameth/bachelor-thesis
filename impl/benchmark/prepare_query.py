62a42791c1c3f37e7529bdabc2da8266ff4df7ac
Sameth <jarkopet@gmail.com>
2016-05-10 17:51:44 +0200

    chystanie query

---
+++ impl/benchmark/prepare_query.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+import random, sys
+qlen = int(sys.argv[1]);
+
+chars = "ACGT"
+try:
+    while(True):
+        seq = input();
+        seq = input();
+        x = random.randint(0, len(seq) - qlen - 1)
+        print(seq[x:x+qlen])
+        randomseq = ""
+        for i in range(qlen):
+            randomseq += random.choice(chars)
+        print(randomseq)
+
+
+except EOFError:
+    pass
