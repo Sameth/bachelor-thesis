402f8dca4d663ca759070a6249aa8c47737a2cc3
Jaroslav Petrucha <sameth@compbio.fmph.uniba.sk>
2016-05-15 14:47:50 +0200

    Na koniec prida 5% random queries

--- impl/benchmark/prepare_query.py
+++ impl/benchmark/prepare_query.py
@@ -3,17 +3,19 @@ import random, sys
 qlen = int(sys.argv[1]);
 
 chars = "ACGT"
+outputs = 0
 try:
     while(True):
         seq = input();
         seq = input();
         x = random.randint(0, len(seq) - qlen - 1)
         print(seq[x:x+qlen])
-        randomseq = ""
-        for i in range(qlen):
-            randomseq += random.choice(chars)
-#        print(randomseq)
+        outputs += 1
 
 
 except EOFError:
-    pass
+    for x in range(outputs//19):
+        randomseq = ""
+        for i in range(qlen):
+            randomseq += random.choice(chars)
+        print(randomseq)
